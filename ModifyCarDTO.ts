499654b59b71ca307d42f3379d6c7d4d23b47aa2
Álvaro <alvaro.peralta.calvo@gmail.com>
2026-03-16 10:52:26 +0100

    Add ModifyCarDTO.ts

---
+++ ModifyCarDTO.ts
@@ -0,0 +1,10 @@
+// File content for ModifyCarDTO.ts
+
+// Defining the DTO for modifying a car
+export class ModifyCarDTO {
+    id: string;
+    make: string;
+    model: string;
+    year: number;
+    price: number;
+}
